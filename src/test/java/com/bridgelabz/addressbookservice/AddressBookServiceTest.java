a38f0d69e84a585582e3f6825474edf6ba7946ed
Ritu1698 <biswasrituparna83@gmail.com>
2020-11-01 11:48:14 +0530

    [Rituparna]Added: Unit Test to Update Address In DB

--- src/test/java/com/bridgelabz/addressbookservice/AddressBookServiceTest.java
+++ src/test/java/com/bridgelabz/addressbookservice/AddressBookServiceTest.java
@@ -8,11 +8,20 @@ import java.util.List;
 
 public class AddressBookServiceTest {
     @Test
-    public void readData() throws SQLException {
+    public void givenContactData_whenRetrieved_shouldMatchSize() throws SQLException {
         AddressBookService addressBookService = new AddressBookService();
         List<Contact> contactList = addressBookService.readContactData();
         Assert.assertEquals(4, contactList.size());
 
+    }
+
+    @Test
+    public void givenNewAddress_whenUpdated_shouldSyncWithDB() throws SQLException {
+        AddressBookService addressBookService = new AddressBookService();
+        List<Contact> contacts = AddressBookService.readContactData();
+        addressBookService.updateContactsAddress("Ritu", "Anushaktinagar");
+        boolean result = addressBookService.checkIfDataBaseIsInSync("Ritu");
+        Assert.assertTrue(result);
     }
 
 }
