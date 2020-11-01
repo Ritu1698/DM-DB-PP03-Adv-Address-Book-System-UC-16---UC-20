be4659438422c3d41efd31c2392c391a2bcaa3cd
Ritu1698 <biswasrituparna83@gmail.com>
2020-11-01 11:32:38 +0530

    [Rituparna]Added: Unit Test to Read Data From DB

--- src/test/java/com/bridgelabz/addressbookservice/AddressBookServiceTest.java
+++ src/test/java/com/bridgelabz/addressbookservice/AddressBookServiceTest.java
@@ -1,4 +1,18 @@
 package com.bridgelabz.addressbookservice;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class AddressBookServiceTest {
+    @Test
+    public void readData() throws SQLException {
+        AddressBookService addressBookService = new AddressBookService();
+        List<Contact> contactList = addressBookService.readContactData();
+        Assert.assertEquals(4, contactList.size());
+
+    }
+
 }
